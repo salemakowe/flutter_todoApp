007368ab1e6910340887ce8ec0479330ab9cc8ef
Salem Akowe <salemakowe@gmail.com>
2022-11-25 16:22:31 +0100

    .

---
+++ todoapp/lib/screen/home.dart
@@ -0,0 +1,24 @@
+import "package:flutter/material.dart";
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "TodoApp",
+          style: GoogleFonts.libreBaskerville(),
+        ),
+      ),
+    );
+  }
+}
