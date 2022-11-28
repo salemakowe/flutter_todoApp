0d860ae2a7e42063eedec2c7613711a284a4f5cd
Salem Akowe <salemakowe@gmail.com>
2022-11-28 02:25:38 +0100

    bottom Nav Bar

--- todoapp/lib/screen/home.dart
+++ todoapp/lib/screen/home.dart
@@ -10,7 +10,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final items = ["Item 1", "Item 2", "Item 3", "Item 4"];
+  final items = [
+    "Item 1",
+    "Item 2",
+    "Item 3",
+    "Item 4",
+    "Item 5",
+    "Item 6",
+    "Item 7",
+    "Item 8",
+    "Item 9",
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -19,26 +29,46 @@ class _HomePageState extends State<HomePage> {
       //appBar
       appBar: todoAppBar(),
 
+      drawer: Drawer(
+        width: MediaQuery.of(context).size.width * 0.8,
+      ),
+
       //the body
-      body: ReorderableListView(
-        onReorder: (oldIndex, newIndex) {
-          setState(() {
-            if (newIndex > oldIndex) newIndex--;
-            final item = items.removeAt(oldIndex);
-            items.insert(newIndex, item);
-          });
-        },
-        children: [
-          for (final item in items)
-            ListTile(
-              key: ValueKey(item),
-              title: Text(item),
-            ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: ReorderableListView(
+          onReorder: (oldIndex, newIndex) {
+            setState(() {
+              if (newIndex > oldIndex) newIndex--;
+              final item = items.removeAt(oldIndex);
+              items.insert(newIndex, item);
+            });
+          },
+          children: [
+            for (final item in items)
+              ListTile(
+                key: ValueKey(item),
+                title: Text(item),
+              ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: "Add Task",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
         ],
       ),
     );
   }
 
+  //Widgets
   //---------------------------
 // AppBar customization
   AppBar todoAppBar() {
@@ -52,26 +82,25 @@ class _HomePageState extends State<HomePage> {
       ),
       elevation: 0,
       backgroundColor: todoBGColor,
+      iconTheme: const IconThemeData(color: todoBlack),
 
       //IconButton
-      leading: IconButton(
-        onPressed: () {},
-        icon: const Icon(
-          Icons.menu,
-          color: todoBlack,
-        ),
-      ),
+      // leading: IconButton(
+      //   onPressed: () {},
+      //   icon: const Icon(
+      //     Icons.menu,
+      //     color: todoBlack,
+      //   ),
+      // ),
 
       //for display picture
       actions: [
-        // ClipRRect(
-        //   borderRadius: BorderRadius.circular(50),
-        //   child: Image.asset(
-        //     "assets/img/avatar.jpeg",
-        //     height: 20,
-        //     width: 20,
-        //   ),
-        // ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(50),
+          child: Image.asset(
+            "assets/img/avatar.jpeg",
+          ),
+        ),
       ],
     );
   }
